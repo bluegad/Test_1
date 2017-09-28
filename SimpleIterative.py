e18878cf38e34e92b52c9ad5e4dcf8494c84efaa
Omar Mohammed <bluegad8@gmail.com>
2017-09-28 03:09:47 +0300

    Added a SimpleIterative.py

---
+++ SimpleIterative.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 02:36:54 2017
+
+@author: mbrk
+"""
+
+for i in range(1,11):
+    if i % 2 == 0:
+        print(i, 'ayoNOOO')
+    else:
+        print(i)
